6fdb940326ec7a01b5e267f454d307ac59eed113
Kukimind <oleg37511@mail.ru>
2019-12-12 23:26:33 +0300

    rot

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("HELLO");
+function sendForm(e){
+     
+    // получаем значение поля key
+    var keyBox = document.search.key;
+    var val = keyBox.value;
+    if(val.length>5){
+        alert("Недопустимая длина строки");
+        e.preventDefault();
+    }   
+    else
+        alert("Отправка разрешена");
+}
+ 
+var sendButton = document.search.send;
+sendButton.addEventListener("click", sendForm);
